c2a3875bdc38c0c7b67c62ea90e2e85eeed48cbe
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-07-09 18:00:33 +0100

    data setup id

--- data.js
+++ data.js
@@ -1,8 +1,9 @@
- const array=[
-    {name: "test1", age: 20, gender: "male"},
-    {name: "test2", age: 15, gender: "female"},
-    {name: "test3", age: 25, gender: "male"},
-     {name: "test4", age: 22, gender: "male"},
-     {name: "test5", age: 29, gender: "female"}]
+const People = [
+  { id: 1, name: "test1", age: 20, gender: "male" },
+  { id: 2, name: "test2", age: 15, gender: "female" },
+  { id: 3, name: "test3", age: 25, gender: "male" },
+  { id: 4, name: "test4", age: 22, gender: "male" },
+  { id: 5, name: "test5", age: 29, gender: "female" },
+];
 
-  module.exports= array;
+module.exports = People;
